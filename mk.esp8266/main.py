93ce54e95bbb08182aaf43297e4b31b2039ad6d9
Martin Kattner <martin.kattner@stygs.com>
2020-01-02 13:36:31 +0100

    update

--- mk.esp8266/main.py
+++ mk.esp8266/main.py
@@ -5,7 +5,7 @@
 
 import machine, time, os
 
-def sub_cb(topic, payload):
+def mqtt_callback(topic, payload):
   global SWITCH_0_SET_TOPIC, SWITCH_0_STATE, SWITCH_0
   print("Callback: " + str(topic) + " - " + str(payload))
   
@@ -26,7 +26,7 @@ def sub_cb(topic, payload):
 def connect_MQTT():
   global mqtt_client_id, mqtt_server, mqtt_port, mqtt_user, mqtt_password
   client = MQTTClient(mqtt_client_id, mqtt_server, mqtt_port, mqtt_user, mqtt_password)
-  client.set_callback(sub_cb)
+  client.set_callback(mqtt_callback)
   client.connect()
   print("Connected")
   return client
@@ -174,118 +174,6 @@ if BME_280_0_load == True:
   print(BME280_0_CONFIG_TOPIC_P)
   print(BME280_0_CONFIG_PAYLOAD_P)
   client.publish(BME280_0_CONFIG_TOPIC_P, BME280_0_CONFIG_PAYLOAD_P)
-  
-
-last_message_1s = 0
-last_message_5s = 0
-last_message_10s = 0
-last_message_20s = 0
-last_message_30s = 0
-last_message_60s = 0
 
 
-while True:
-  # inizialize
-  DS1820B_0_temp = 0
-  try:
-    
-    
-    client.check_msg()
-    
-    if (time.time() - last_message_1s) > 1:
-      # ________________________
-      #     every 1 second
-      # ------------------------
-      print("every 1 second")
-      last_message_1s = time.time()
-    
-    elif (time.time() - last_message_5s) > 5:
-      # ________________________
-      #     every 5 seconds
-      # ------------------------
-      print("every 5 seconds")
-      
-
-      if SWITCH_0_load == True:
-        print(SWITCH_0_STATE)
-        # SWITCH state update
-        value = SWITCH_0.value()
-        if value == 0:
-          SWITCH_0_STATE = b"ON"
-        elif value == 1:
-          SWITCH_0_STATE = b"OFF"
-        print(SWITCH_0_STATE_TOPIC + " - " + SWITCH_0_STATE)
-        client.publish(SWITCH_0_STATE_TOPIC, SWITCH_0_STATE)
-      
-      last_message_5s = time.time()
-      
-    if (time.time() - last_message_10s) > 10:
-      # ________________________
-      #     every 10 seconds
-      # ------------------------
-      print("every 10 seconds");
-      last_message_10s = time.time()
-      
-    if (time.time() - last_message_20s) > 20:
-      # ________________________
-      #     every 20 seconds
-      # ------------------------
-      print("every 20 seconds");
-      last_message_20s = time.time()
-      
-    if (time.time() - last_message_30s) > 30:
-      # ________________________
-      #     every 30 seconds
-      # ------------------------
-      print("every 30 seconds");
-      last_message_30s = time.time()
-      
-    if (time.time() - last_message_60s) > 60:
-      # ________________________
-      #     every 60 seconds
-      # ------------------------
-      print("every 60 seconds");
-
-
-      # DS1820B update
-      if DS1820B_0_load == True:
-        DS1820B.convert_temp()
-        time.sleep_ms(750) #Note that you must execute the convert_temp() function to initiate a temperature reading, then wait at least 750ms before reading the value.
-        if  DS1820B_0_temp != round(DS1820B.read_temp(DS1820B_0), 1):
-          DS1820B_0_temp = round(DS1820B.read_temp(DS1820B_0), 1)
-          print(DS1820B_0_temp)
-          DS1820B_0_msg = '{ "temperature":' + str(DS1820B_0_temp) + '}'
-          print(DS1820B_0_msg)
-          client.publish(DS1820B_0_STATE_TOPIC, DS1820B_0_msg)
-      
-      # ---
-      
-      # DHT11 update
-      if DHT11_0_load == True:
-        DHT11_0.measure()
-        DHT11_0_msg = '{ "temperature":' + str(DHT11_0.temperature()) + ', "humidity":' + str(DHT11_0.humidity()) + '}'
-        print(DHT11_0_msg)
-        client.publish(DHT11_0_STATE_TOPIC, DHT11_0_msg)
-      
-      # ---
-      
-      #BME280 update
-      if BME_280_0_load == True:
-        BME280_0_msg = '{ "temperature":' + bme.raw_temperature + ', "humidity":' + bme.raw_humidity + ', "pressure":' + bme.raw_pressure + '}'
-        print(BME280_0_msg)
-        client.publish(BME280_0_STATE_TOPIC, BME280_0_msg)
-
-
-      last_message_60s = time.time()
-      
-  except OSError as e:
-    print(e)
-    #f = open('data.txt', 'w')
-    #f.write(e)
-    #f.close()
-    restart_and_reconnect()
-    
-  except Exception as e:
-    print("Strange error")
-    print(e)
-    restart_and_reconnect()
+import loop
